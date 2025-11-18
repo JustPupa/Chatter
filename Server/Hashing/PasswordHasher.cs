b3e0609e6d61020f00c3abf0e57d5dbc62513ae5
JustPupa <andrey_feshchukov@mail.ru>
2025-11-18 22:45:27 +0300

    argon2id hashing added

---
+++ Server/Hashing/PasswordHasher.cs
@@ -0,0 +1,37 @@
+﻿using Isopoh.Cryptography.Argon2;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cozy_Chatter.Hashing
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            // Генерируем случайную соль
+            byte[] salt = RandomNumberGenerator.GetBytes(16);
+
+            var config = new Argon2Config
+            {
+                Type = Argon2Type.DataIndependentAddressing,
+                Version = Argon2Version.Nineteen,
+                TimeCost = 4,
+                MemoryCost = 1024 * 64,
+                Lanes = 4,
+                Threads = 4,
+                Password = Encoding.UTF8.GetBytes(password),
+                Salt = salt,
+                HashLength = 32,
+            };
+
+            using var argon2 = new Argon2(config);
+            var hash = argon2.Hash();
+
+            return config.EncodeString(hash.Buffer);
+        }
+        public static bool VerifyPassword(string password, string encodedHash)
+        {
+            return Argon2.Verify(encodedHash, password);
+        }
+    }
+}
